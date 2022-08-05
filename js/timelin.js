d6e0b4be1cbb7c791797f3363e173b224932bf8a
Shawn Wang <shawn.won@hotmail.com>
2022-08-05 14:23:15 +0800

    'aboutmeRedo'

--- js/timelin.js
+++ js/timelin.js
@@ -83,7 +83,9 @@ var swiper = new Swiper(".swiperaboutme", {
 var scroll = window.requestAnimationFrame ||
 function(callback){ window.setTimeout(callback, 1000/60)};
 var elementsToShow = document.querySelectorAll('.show-scroll'); 
-
+var lineToAnima = document.querySelectorAll('.card__line');
+var imageToAnima = document.querySelectorAll('.card__image');
+var contentToAnima = document.querySelectorAll('.card__content');
 function loop() {
 
   Array.prototype.forEach.call(elementsToShow, function(element){
@@ -91,6 +93,30 @@ function loop() {
       element.classList.add('scroll-visible');
     } else {
       element.classList.remove('scroll-visible');
+    }
+  });
+
+  Array.prototype.forEach.call(lineToAnima, function(element){
+    if (isElementInViewport(element)) {
+      element.classList.add('card__line__anima');
+    } else {
+      element.classList.remove('card__line__anima');
+    }
+  });
+
+  Array.prototype.forEach.call(imageToAnima, function(element){
+    if (isElementInViewport(element)) {
+      element.classList.add('card__image__anima');
+    } else {
+      element.classList.remove('card__image__anima');
+    }
+  });
+
+  Array.prototype.forEach.call(contentToAnima, function(element){
+    if (isElementInViewport(element)) {
+      element.classList.add('card__content__anima');
+    } else {
+      element.classList.remove('card__content__anima');
     }
   });
 
