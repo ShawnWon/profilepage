f483f09239d91a8fe9ce2764490dffa6b9f6ae89
ShawnWon <47803938+ShawnWon@users.noreply.github.com>
2022-08-12 09:13:13 +0800

    'imagesloadingsnag'

--- js/main.js
+++ js/main.js
@@ -151,12 +151,28 @@
 	$(function(){
 		contentWayPoint();
 		goToTop();
-		loaderPage();
+		//loaderPage();
 		fullHeight();
 		parallax();
 		// pieChart();
 		skillsWayPoint();
 		adjustImgRatio();
+		$(document).ready(function(){
+			var imgs = document.images, len = imgs.length, counter =0;
+			[].forEach.call(imgs, function(img){
+				if(img.complete) incrementCounter();
+				else
+					img.addEventListener('load', incrementCounter, false);
+			});
+	
+			function incrementCounter(){
+				counter++;
+				if(counter===len){
+					loaderPage();
+				}
+			}
+		});
+
 	});
 
 	var words = ['Software Developer', 'System Analyst', 'Cyclist', 'Photographer'],
